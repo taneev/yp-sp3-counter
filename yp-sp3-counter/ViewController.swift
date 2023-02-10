93d578ea697569012d4501e054bc2b0eb5753b75
Timur Taneev <taneev@yandex.ru>
2023-02-10 22:52:52 +0100

    Постановка задачи, инициализация проекта

---
+++ yp-sp3-counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yp-sp3-counter
+//
+//  Created by Тимур Танеев on 10.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
