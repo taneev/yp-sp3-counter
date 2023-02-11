d3936490674a2f488c713b89d2babc19701b62b9
Timur Taneev <taneev@yandex.ru>
2023-02-11 18:16:36 +0100

    Добавлена кнопка, лейбл, обновление лейбла по нажатию кнопки

--- yp-sp3-counter/ViewController.swift
+++ yp-sp3-counter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var counter: Int = 0
+
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateCounterLabel()
     }
 
-
+    @IBAction func tapCounterButton(_ sender: Any) {
+        counter += 1
+        updateCounterLabel()
+    }
+    
+    private func updateCounterLabel() {
+        counterLabel.text = "Значение счетчика: \(counter.description)"
+    }
 }
 
